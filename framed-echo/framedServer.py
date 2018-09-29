0c1efd2bb29b7bf91e690599e278f0a60a4e43f0
alexis <anreyes2@miners.utep.edu>
2018-09-29 16:01:27 -0600

    adding new files

--- framed-echo/framedServer.py
+++ framed-echo/framedServer.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python3
 
+import sys, re, socket
 sys.path.append("../lib")       # for params
-import sys, re, socket, params
+import params
 
 switchesVarDefaults = (
     (('-l', '--listenPort') ,'listenPort', 50001),
